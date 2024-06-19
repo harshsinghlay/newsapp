fb79f12e9247ae88607776d36933562eebd40c8a
harshsinghlay <harshsinghlay9990@example.com>
2024-06-19 13:00:32 -0500

    add currentsApi

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,16 @@ function App() {
   const [data, setData] = useState([])
 
   useEffect(() => {
-    const apiKey = '18b97f8e-01b5-4ffe-afde-b0d060aeffda';
-    const url = `https://content.guardianapis.com/search?q=football&api-key=${apiKey}`
+    const apiKey = 'xlQcHfAxU_JbL1qXBsEzfJM-np-Co_HJJ3u2FyqelGQJc3-M';
+    let url = 'https://api.currentsapi.services/v1/latest-news?' +
+    'language=en&' +
+    `apiKey=${apiKey}`;
 
     fetch(url)
       .then((res) => res.json())
       .then((response) => {
-        console.log("Response is", response.response.results);
-        setData(response.response.results)
+        console.log("Response is", response);
+        setData(response.news)
       })
       .catch((error) => {
         console.error('Error:', error);
@@ -21,19 +23,19 @@ function App() {
 
   return (
     <>
-    <h1 className='bg-gray-600 text-white text-center py-4 mb-2'>App is Running</h1>
-      {data?.length > 0 ? 
-      <div className='w-full h-full grid grid-cols-3 gap-4'>
-      {data.map((item,index) => (
-        <Card
-          key={index}
-          title={item.pillarName}
-          content={item.webTitle}
-          img={item.webUrl}
-        />
-      ))}
-    </div> : 
-    <div className='h-[70vh] bg-orange-200 flex justify-center items-center text-xl'>No Data found</div>
+      <h1 className='bg-gray-600 text-white text-center py-4 mb-2'>App is Running</h1>
+      {data?.length > 0 ?
+        <div className='w-full h-full grid grid-cols-3 gap-4'>
+          {data?.map((item, index) => (
+            <Card
+              key={index}
+              title={item.title}
+              content={item.description}
+              img={item.image}
+            />
+          ))}
+        </div> :
+        <div className='h-[70vh] bg-orange-200 flex justify-center items-center text-xl'>No Data found</div>
       }
     </>
   );
