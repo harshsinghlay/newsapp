4f14ac959b01d5d750f6c5d595e7004deb23eac9
harshsinghlay <harshsinghlay9990@example.com>
2024-06-19 12:26:36 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ function App() {
   const [data, setData] = useState([])
 
   useEffect(() => {
-    const apiKey = 'ff1ae913181341d7b9e376a80f1dca1d';
-    const url = `https://newsapi.org/v2/top-headlines?country=${'in'}&category=${"general"}&apiKey=${apiKey}&page=${1}&pageSize=${5}`
+    const apiKey = '18b97f8e-01b5-4ffe-afde-b0d060aeffda';
+    const url = `https://content.guardianapis.com/search?q=football&api-key=${apiKey}`
 
     fetch(url)
       .then((res) => res.json())
       .then((response) => {
-        console.log("Response is", response.articles);
-        setData(response.articles)
+        console.log("Response is", response.response.results);
+        setData(response.response.results)
       })
       .catch((error) => {
         console.error('Error:', error);
@@ -24,11 +24,12 @@ function App() {
     <h1 className='bg-gray-600 text-white text-center py-4 mb-2'>App is Running</h1>
       {data?.length > 0 ? 
       <div className='w-full h-full grid grid-cols-3 gap-4'>
-      {data.map((item) => (
+      {data.map((item,index) => (
         <Card
-          title={item.title}
-          content={item.description}
-          img={item.urlToImage}
+          key={index}
+          title={item.pillarName}
+          content={item.webTitle}
+          img={item.webUrl}
         />
       ))}
     </div> : 
