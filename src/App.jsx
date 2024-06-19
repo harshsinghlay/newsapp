bf52548babd95d73f2c259402f1e8574eb4d8746
harshsinghlay <harshsinghlay9990@example.com>
2024-06-19 12:00:35 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react';
+import Card from './components/Card';
+
+function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const apiKey = 'ff1ae913181341d7b9e376a80f1dca1d';
+    const url = `https://newsapi.org/v2/top-headlines?country=${'in'}&category=${"general"}&apiKey=${apiKey}&page=${1}&pageSize=${5}`
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((response) => {
+        console.log("Response is", response.articles);
+        setData(response.articles)
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }, []);
+
+  return (
+    <>
+    <h1 className='bg-gray-600 text-white text-center py-4 mb-2'>App is Running</h1>
+      {data.length > 0 ? 
+      <div className='w-full h-full grid grid-cols-3 gap-4'>
+      {data.map((item) => (
+        <Card
+          title={item.title}
+          content={item.description}
+          img={item.urlToImage}
+        />
+      ))}
+    </div> : 
+    <div className='h-[70vh] bg-orange-200 flex justify-center items-center text-xl'>No Data found</div>
+      }
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+function Card({ id = "null", title = "null", content = "null", category = "null", date = "null", img = "null" }) {
+    return (
+        <div className='w-full h-full  group'>
+            <div className="w-full h-full rounded overflow-hidden flex flex-col">
+                {/*============= Blog Image =============*/}
+                <div className="relative h-[50%] overflow-hidden rounded-tl-sm rouned-tr-sm"><div >
+                    <img className="w-full lg:group-hover:scale-110  transition-transform duration-300"
+                        src={img}
+                        alt="blog Image" />
+                </div>
+                    <div className='overflow-hidden '>
+                        <p
+                            className="text-xs absolute top-0 right-0 min-w-[30%] max-w-[40%] bg-indigo-600 px-4 py-2 text-white mt-3 mr-3 lg:hover:bg-white lg:hover:text-indigo-600 transition duration-500 ease-in-out">
+                            <span className='  line-clamp-1 text-center'>
+                                {category || "category"}
+                            </span>
+                        </p>
+                    </div>
+                </div>
+                {/*============= Blog Content =============*/}
+                <div className='flex flex-col justify-between h-[50%] mx-auto w-[92%] overflow-hidden'>
+                    <div className="pt-4  ">
+                        <p
+                            className="line-clamp-1 text-nowrap overflow-ellipsis  pb-1 font-medium text-lg lg:hover:text-indigo-600 transition duration-500 ease-in-out inline-block">
+                            {title}
+                        </p>
+                        <p className="text-gray-500 text-sm line-clamp-4">
+                            {content}
+                        </p>
+                    </div>
+                    <div className=" pt-3 pb-2 flex flex-row items-center justify-between ">
+                        <span className="py-1 text-sm font-regular text-gray-900 mr-1 flex flex-row items-center">
+                            <span className="ml-1">{date}</span>
+                        </span>
+                        <span className="py-1 text-sm font-regular text-gray-900 mr-1 flex flex-row items-center lg:hover:text-indigo-600">
+                            <span className="ml-1">Read More. </span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card
