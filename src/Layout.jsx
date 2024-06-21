3b6c19e80a806f374fc6983a9cd13e59b34e6bfb
unknown <harshsinghlay9990@gmail.com>
2024-06-21 17:00:12 +0530

    first commit

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 function Layout() {
-
-    return (
-        <div className='bg-white w-full'>
-            <Header />
-            <Outlet />
-            <Footer />
-        </div>
-    )
+  return (
+    <div className="bg-white w-full">
+      <Header />
+      <Outlet />
+      <ScrollToTop />
+      <Footer />
+    </div>
+  );
 }
 
-export default Layout
+export default Layout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import Layout from './Layout.jsx'
-import Article from './components/Article.jsx'
-import Home from './components/Home.jsx'
-import store from './redux/store.jsx'
-import About from './components/About.jsx'
-import Contact from './components/Contact.jsx'
-import Error from './components/Error.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import { Provider } from "react-redux";
+import Layout from "./Layout.jsx";
+import Article from "./components/Article.jsx";
+import Home from "./components/Home.jsx";
+import store from "./redux/store.jsx";
+import About from "./components/About.jsx";
+import Contact from "./components/Contact.jsx";
+import Error from "./components/Error.jsx";
 
-const router = createBrowserRouter(createRoutesFromElements(
-  <Route path='/' element={<Layout />}>
-    <Route index element={<Home />} />
-    <Route path="about" element={<About />} />
-    <Route path="contact" element={<Contact />} />
-    <Route path='article/:id' element={<Article />} />
-    <Route path="*" element={<Error />} />
-  </Route>
-))
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="article/:id" element={<Article />} />
+      <Route path="*" element={<Error />} />
+    </Route>
+  )
+);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <RouterProvider router={router} />
   </Provider>
-)
+);
